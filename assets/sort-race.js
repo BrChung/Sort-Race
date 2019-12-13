00386ac2a469ce4f5309cd7b8e74df00c65e8ab3
Brian Chung <35940686+BrChung@users.noreply.github.com>
2019-12-13 00:51:53 -0800

    Update sort-race.js

--- assets/sort-race.js
+++ assets/sort-race.js
@@ -6,10 +6,10 @@ Last Modified:
 ------------------------------------------------------------------------*/
 
 //For Blue:
-//For all the get__SortAnimations, given an unsorted array, the function will console.log the sorted array and return a list called animations
-//The animations list is what our race manager will use to print the arrays. For our problem we will need to change the implementation for the animation.push values
+//For all the get__SortAnimations, given an unsorted array, the function will console.log the sorted array and return a list called steps
+//The steps list is what our race manager will use to print the arrays. For our problem we will need to change the implementation for the animation.push values
 //For instance, we may want to push the following animation[step, index, swap]
-//The step will tell the manager what step we're currently on, all animations on step 1 will display on the first row
+//The step will tell the manager what step we're currently on, all steps on step 1 will display on the first row
 //The index will obvious tell the manger what index so that the manager knows what index to change color
 //The swap will be a bool, true refers to a swap and false refers to a comparison. Each will be represented with a different color
 
@@ -20,129 +20,220 @@ Last Modified:
 
 //Since we're not using Node we're limited to just this javascript file and the drawing functions of P5
 
+var MergeSortSteps = []
+var QuickSortSteps = []
+var SelectionSortSteps = []
+var hexUsed = true;
+var GivenArray = [0, 'b', 'a', 3, 2, 8, 4, 7, 6, 5, 1, 9]
+var passedArray = GivenArray.slice()
+
+//Convert all values into ints to put into sorting algo
+if(hexUsed){
+  for(let i = 0; i < passedArray.length; i++){
+    passedArray[i] = parseInt(passedArray[i], 16);
+  }
+}
+
+console.log(passedArray);
+
+
+//Race Manager
+function raceManager(){
+  MergeSortSteps = getMergeSortSteps(passedArray.slice());
+  QuickSortSteps = getQuickSortSteps(passedArray.slice());
+  SelectionSortSteps = getSelectionSortSteps(passedArray.slice());
+}
+
+//MergeSort GUI
+function drawMergeSortSteps(){
+  mergeSortArray = GivenArray.slice();
+  for(let i=0;i<MergeSortSteps.length;i++){
+    const isComparison = MergeSortSteps[i][2];
+    if(isComparison){
+      const idx1 = MergeSortSteps[i][0];
+      const idx2 = MergeSortSteps[i][1];
+      if(idx1 === idx2){
+        //Pivot value?
+      }
+      else{
+      //pass but make 
+      }
+    }
+    else{
+      const idx = MergeSortSteps[i][0];
+      const idxHeight = MergeSortSteps[i][1];
+      if(hexUsed){
+        if(idxHeight > 9){
+          let hexString = idxHeight.toString(16);
+          mergeSortArray[idx] = hexString;
+        }
+        else{
+          mergeSortArray[idx] = idxHeight;
+        }
+      }
+      else{
+        mergeSortArray[idx] = idxHeight;
+      }
+    }
+    console.log(mergeSortArray)
+  }
+}
+
+//QuickSort GUI
+function drawQuickSortSteps(){
+  quickSortArray = GivenArray.slice();
+  for(let i=0;i<QuickSortSteps.length;i++){
+    const isComparison = QuickSortSteps[i][2];
+    if(isComparison){
+      const idx1 = QuickSortSteps[i][0];
+      const idx2 = QuickSortSteps[i][1];
+      //
+    }
+    else{
+      const idx1 = QuickSortSteps[i][0];
+      const idx2 = QuickSortSteps[i][1];
+      swap(quickSortArray, idx1, idx2)
+    }
+    console.log(quickSortArray)
+  }
+}
+
+//Selection Sort GUI
+function drawSelectionSortSteps(){
+  selectionSortArray = GivenArray.slice();
+  for(let i=0;i<SelectionSortSteps.length;i++){
+    const isComparison = SelectionSortSteps[i][2];
+    if(isComparison){
+      const idx1 = SelectionSortSteps[i][0];
+      const idx2 = SelectionSortSteps[i][1];
+      //pass but make 
+    }
+    else{
+      const idx1 = SelectionSortSteps[i][0];
+      const idx2 = SelectionSortSteps[i][1];
+      swap(selectionSortArray, idx1, idx2)
+    }
+    console.log(selectionSortArray)
+  }
+}
+
+
 
 //Sorting Algorithms
 //Merge Sort
-function getMergeSortAnimations(array) {
-    const animations = [];
-    if (array.length <= 1) return array;
-    const auxiliaryArray = array.slice();
-    mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, animations);
-    console.log(array);
-    return animations;
+function getMergeSortSteps(array) {
+  const steps = [];
+  if (array.length <= 1) return array;
+  const auxiliaryArray = array.slice();
+  mergeSortHelper(array, 0, array.length - 1, auxiliaryArray, steps);
+  return steps;
 }
   
-function mergeSortHelper(mainArray, startIdx, endIdx, auxiliaryArray, animations) {
-    if (startIdx === endIdx) return;
-    const middleIdx = Math.floor((startIdx + endIdx) / 2);
-    mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, animations);
-    mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, animations);
-    doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations);
+function mergeSortHelper(mainArray, startIdx, endIdx, auxiliaryArray, steps) {
+  if (startIdx === endIdx) return;
+  const middleIdx = Math.floor((startIdx + endIdx) / 2);
+  mergeSortHelper(auxiliaryArray, startIdx, middleIdx, mainArray, steps);
+  mergeSortHelper(auxiliaryArray, middleIdx + 1, endIdx, mainArray, steps);
+  doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, steps);
 }
   
-function doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, animations) {
-    let k = startIdx;
-    let i = startIdx;
-    let j = middleIdx + 1;
-    while (i <= middleIdx && j <= endIdx) {
-      animations.push([i, j]);
-      animations.push([i, j]);
-      if (auxiliaryArray[i] <= auxiliaryArray[j]) {
-        animations.push([k, auxiliaryArray[i]]);
-        mainArray[k++] = auxiliaryArray[i++];
-      } else {
-        animations.push([k, auxiliaryArray[j]]);
-        mainArray[k++] = auxiliaryArray[j++];
-      }
-    }
-    while (i <= middleIdx) {
-      animations.push([i, i]);
-      animations.push([i, i]);
-      animations.push([k, auxiliaryArray[i]]);
+function doMerge(mainArray, startIdx, middleIdx, endIdx, auxiliaryArray, steps) {
+  let k = startIdx;
+  let i = startIdx;
+  let j = middleIdx + 1;
+  while (i <= middleIdx && j <= endIdx) {
+    steps.push([i, j, true]);
+    if (auxiliaryArray[i] <= auxiliaryArray[j]) {
+      steps.push([k, auxiliaryArray[i], false]);
       mainArray[k++] = auxiliaryArray[i++];
-    }
-    while (j <= endIdx) {
-      animations.push([j, j]);
-      animations.push([j, j]);
-      animations.push([k, auxiliaryArray[j]]);
+    } else {
+      steps.push([k, auxiliaryArray[j], false]);
       mainArray[k++] = auxiliaryArray[j++];
     }
   }
+  while (i <= middleIdx) {
+    steps.push([i, i, true]);
+    steps.push([k, auxiliaryArray[i], false]);
+    mainArray[k++] = auxiliaryArray[i++];
+  }
+  while (j <= endIdx) {
+    steps.push([j, j, true]);
+    steps.push([k, auxiliaryArray[j], false]);
+    mainArray[k++] = auxiliaryArray[j++];
+  }
+}
 
 //Quick Sort
-function getQuickSortAnimations(array) {
-    const animations = [];
-    if (array.length <= 1) return array;
-    const auxiliaryArray = array.slice();
-    doQuickSort(auxiliaryArray, 0, array.length - 1, animations);
-    console.log(auxiliaryArray);
-    return animations;
+function getQuickSortSteps(array) {
+  const steps = [];
+  if (array.length <= 1) return array;
+  const auxiliaryArray = array.slice();
+  doQuickSort(auxiliaryArray, 0, array.length - 1, steps);
+  return steps;
 }
   
-function doQuickSort(array, startIdx, endIdx, animations) {
-    if (startIdx >= endIdx){
-      return;
-    }
-    const pivotIdx = partitionArray(array, startIdx, endIdx, animations);
-    doQuickSort(array, startIdx, pivotIdx-1, animations);
-    doQuickSort(array, pivotIdx+1, endIdx, animations);
+function doQuickSort(array, startIdx, endIdx, steps) {
+  if (startIdx >= endIdx){
+    return;
+  }
+  const pivotIdx = partitionArray(array, startIdx, endIdx, steps);
+  doQuickSort(array, startIdx, pivotIdx-1, steps);
+  doQuickSort(array, pivotIdx+1, endIdx, steps);
 }
   
-function partitionArray(array, startIdx, endIdx, animations){
-    // pivot (Element to be placed at right position) also the last element
-    let pivotValue = array[endIdx];
-    let pivotIdx = startIdx; // Index of smaller element
-    for (let i=startIdx; i<endIdx; i++){
-      // If current element is smaller than the pivot
-      if (array[i] < pivotValue){
-        animations.push([i, pivotIdx, true]);
-        animations.push([i, pivotIdx, true]);
-        animations.push([i, array[pivotIdx], false]);
-        animations.push([pivotIdx, array[i], false]);
-        swap(array, i, pivotIdx);
-        pivotIdx++; // increment index of smaller element
-      }
+function partitionArray(array, startIdx, endIdx, steps){
+  // pivot (Element to be placed at right position) also the last element
+  let pivotValue = array[endIdx];
+  let pivotIdx = startIdx; // Index of smaller element
+  for (let i=startIdx; i<endIdx; i++){
+    // If current element is smaller than the pivot
+    if (array[i] < pivotValue){
+      steps.push([i, pivotIdx, true]);
+      steps.push([i, pivotIdx, false]);
+      swap(array, i, pivotIdx);
+      pivotIdx++; // increment index of smaller element
     }
-    animations.push([pivotIdx, endIdx, true]);
-    animations.push([pivotIdx, endIdx, true]);
-    animations.push([pivotIdx, array[endIdx], false]);
-    animations.push([endIdx, array[pivotIdx], false]);
-    swap(array, pivotIdx, endIdx);
-    return(pivotIdx);
+  }
+  steps.push([pivotIdx, endIdx, true]);
+  steps.push([pivotIdx, endIdx, false]);
+  swap(array, pivotIdx, endIdx);
+  return(pivotIdx);
 }
 
 //Selection Sort
-export function getSelectionSortAnimations(array){
-    const animations=[];
-    if(array.length<=1)return array;
-    const auxiliaryArray=array.slice();
-    doSelectionSort(auxiliaryArray,animations);
-    console.log(auxiliaryArray);
-    return animations;
+function getSelectionSortSteps(array){
+  const steps=[];
+  if(array.length<=1)return array;
+  const auxiliaryArray=array.slice();
+  doSelectionSort(auxiliaryArray,steps);
+  return steps;
 }
   
-function doSelectionSort(array, animations){
-    let minIdx;
-    let len = array.length;
-    for(let i=0; i < len; i++){
-      minIdx = i;
-      for(let j = i+1; j < len; j++){
-        if(array[j] < array[minIdx]){
-          minIdx = j;
-        }
-        animations.push([i, j, true]);
-        animations.push([i, j, true]);
+function doSelectionSort(array, steps){
+  let minIdx;
+  let len = array.length;
+  for(let i=0; i < len; i++){
+    minIdx = i;
+    for(let j = i+1; j < len; j++){
+      if(array[j] < array[minIdx]){
+        minIdx = j;
       }
-      animations.push([i, array[minIdx], false]);
-      animations.push([minIdx, array[i], false]);
-      swap(array, i, minIdx)
+      steps.push([i, j, true]);
     }
-    return array;
+    steps.push([i, minIdx, false]);
+    swap(array, i, minIdx)
+  }
+  return array;
 }
   
 //Swap function for swapping values
 function swap(array, firstIndex, secondIndex){
-    let tempValue = array[firstIndex];
-    array[firstIndex] = array[secondIndex];
-    array[secondIndex] = tempValue;
+  let tempValue = array[firstIndex];
+  array[firstIndex] = array[secondIndex];
+  array[secondIndex] = tempValue;
 }
+
+raceManager();
+//drawQuickSortSteps();
+drawSelectionSortSteps();
+//drawMergeSortSteps();
